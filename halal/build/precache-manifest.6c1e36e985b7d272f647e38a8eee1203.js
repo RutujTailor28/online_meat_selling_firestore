9650aad6f11dd3264ffc4ede49204d21fab927d5
RutujTailor28 <54097465+RutujTailor28@users.noreply.github.com>
2021-10-30 10:44:12 +0530

    Add files via upload

---
+++ halal/build/precache-manifest.6c1e36e985b7d272f647e38a8eee1203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec8ee50a9bf2223362db29d28c763e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7fdacafdb6046485b80",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "773ad2f429974466d1cd",
+    "url": "/static/css/main.f4194186.chunk.css"
+  },
+  {
+    "revision": "e7fdacafdb6046485b80",
+    "url": "/static/js/2.099ec476.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.099ec476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773ad2f429974466d1cd",
+    "url": "/static/js/main.7cce9dc9.chunk.js"
+  },
+  {
+    "revision": "45b2939505673c825eb7",
+    "url": "/static/js/runtime-main.8f8c67a9.js"
+  }
+]);
